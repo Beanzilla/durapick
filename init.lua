d5e3f13826077cb6c9ff15b33ebe2f191efda8c7
Beanzilla <david2bond2@gmail.com>
2021-02-27 00:52:57 -0500

    Found that the default tools were more durable
    Added additional durability too stone, steel (iron), bronze, mese and diamond.

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@
 -- Settings
 
 -- Durabilites
-durapick_durability_wood = 100
-durapick_durability_stone = 200
-durapick_durability_steel = 300
-durapick_durability_bronze = 400
-durapick_durability_mese = 500
-durapick_durability_diamond = 600
+durapick_durability_wood = 100 -- default:pick_wood 30
+durapick_durability_stone = 275 -- default:pick_stone 60
+durapick_durability_steel = 450 -- default:pick_steel 180
+durapick_durability_bronze = 450 -- default:pick_bronze 180
+durapick_durability_mese = 1200 -- default:pick_mese 540
+durapick_durability_diamond = 2500 -- default:pick_diamond 610
 
 -- Resources
 durapick_stick = "default:stick"
